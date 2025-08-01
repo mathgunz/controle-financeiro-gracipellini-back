d07a95a1c2c2f3dbcb38412d81ecb48ae439f836
matheus <mathgunz@gmail.com>
2025-08-01 18:49:46 -0300

    added banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,28 @@ import { PessoaModule } from './pessoa/pessoa.module';
 import { ReceitaModule } from './receita/receita.module';
 import { ResumoModule } from './resumo/resumo.module';
 import { DespesaModule } from './despesa/despesa.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ContaModule, PessoaModule, ReceitaModule, ResumoModule, DespesaModule],
+  imports: [
+    ContaModule,
+    PessoaModule,
+    ReceitaModule,
+    ResumoModule,
+    DespesaModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'postgres',
+      database: process.env.DB_NAME || 'controle_financeiro',
+      autoLoadEntities: true,
+      synchronize: true, // use false em produção!
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
