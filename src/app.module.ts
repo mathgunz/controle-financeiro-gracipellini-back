1559a60ea44b27fa767d127717cc1ba2ca7b3f3d
matheus <mathgunz@gmail.com>
2025-08-06 17:45:42 -0300

    add module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import { ResumoModule } from './resumo/resumo.module';
 import { DespesaModule } from './despesa/despesa.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
+import { Pessoa } from './entities/pessoa.entity';
+import { Receita } from './entities/receita.entity';
+import { Despesa } from './entities/despesa.entity';
 
 @Module({
   imports: [
@@ -22,8 +25,9 @@ import { ConfigModule } from '@nestjs/config';
       username: process.env.DB_USER || 'postgres',
       password: process.env.DB_PASS || 'postgres',
       database: process.env.DB_NAME || 'controle_financeiro',
-      autoLoadEntities: true,
+      // autoLoadEntities: true,
       synchronize: true, // use false em produção!
+      entities: [Pessoa, Receita, Despesa]
     }),],
   controllers: [AppController],
   providers: [AppService],
